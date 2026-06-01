241d76b18baf069de03d51856055e16a34a504d9
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2026-06-01 19:43:11 +0300

    chore: update icon for improved branding consistency

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,9 +1,15 @@
-import logoMoving from "@/public/transit_logos/transit_logo_q.png";
+import logoMoving from "@/public/images/gitalogo.png";
 import Image from "next/image";
 function Loading() {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-white overflow-hidden">
-      <Image src={logoMoving} alt="logo" width={800} height={800} className="w-20 h-20" />
+      <Image
+        src={logoMoving}
+        alt="logo"
+        width={800}
+        height={800}
+        className="w-20 h-20"
+      />
     </div>
   );
 }
